d6397a89a24117dbd56068f564aaf49cb00736df
Paul Mallet <pamallet@c1r6p14.42lehavre.fr>
2025-02-12 16:32:46 +0100

    feat: added pwd functions

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -1,45 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins.h                                         :+:      :+:    :+:   */
+/*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/12 11:43:19 by pamallet          #+#    #+#             */
-/*   Updated: 2025/02/12 16:31:03 by pamallet         ###   ########.fr       */
+/*   Created: 2025/02/12 15:46:29 by pamallet          #+#    #+#             */
+/*   Updated: 2025/02/12 16:31:17 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTINS_H
-# define BUILTINS_H
-# include "./minishell.h"
+#include "../../includes/minishell.h"
+#include "../../includes/builtins.h"
 
-/*
-	builtins handler
-*/
-void	builtins(char *input, t_env *env);
-
-/*
-	pwd
-*/
-void	ft_pwd(void);
-
-/*
-	unset
-*/
-void	unset_env(t_env **env, char *key);
-
-/*
-	env
-*/
-t_env	*new_env_node(char *entry);
-t_env	*import_env(char **env);
-void	print_env(t_env *env);
-void	free_env(t_env *env);
-
-/*
-	exit
-*/
-void	ft_exit(t_env *env);
-
-#endif
+void	builtins(char *input, t_env *env)
+{
+	if (!ft_strcmp(input, "pwd"))
+		ft_pwd();
+	if (!ft_strcmp(input, "exit"))
+		ft_exit(env);
+}
--- src/main.c
+++ src/main.c
@@ -3,45 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:55:18 by pamallet          #+#    #+#             */
-/*   Updated: 2025/02/12 15:00:11 by abarahho         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:30:52 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include "../includes/builtins.h"
 
-// int main(int ac, char **av, char **envp)
-// {
-// 	t_env   *our_env;
-
-// 	(void)ac;
-// 	(void)av;
-// 	our_env = import_env(envp);
-// 	print_env(our_env);
-// 	unset_env(&our_env, "COLORTERM");
-// 	print_env(our_env);
-// 	free_env(our_env);
-// 	return (0);
-// }
-
 int main(int ac, char **av, char **envp)
 {
 	char *input;
-	(void)ac;
-	(void)av;
 	t_env   *env;
 
+	(void)ac;
+	(void)av;
 	env = import_env(envp);
 	while ((input = readline("MiniShell$>")))
 	{
 		if (input) add_history(input);
-			printf("Vous avez tapé: %s\n", input);
+			ft_printf("Vous avez tapé: %s\n", input);
+		builtins(input, env);	
 		free(input);
-		if (!ft_strcmp(input, "exit"))
-			ft_exit(env);
 	}
 	return (0);
 }
