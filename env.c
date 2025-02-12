ec97673782b39a87aeef3ccad8e83b76abe7037a
Adambrhh <adam.barahhou@proton.me>
2025-02-12 10:35:37 +0100

    Upgarding env functions

--- env.c
+++ env.c
@@ -6,33 +6,39 @@
 /*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 18:39:34 by abarahho          #+#    #+#             */
-/*   Updated: 2025/02/11 19:42:47 by abarahho         ###   ########.fr       */
+/*   Updated: 2025/02/12 10:30:35 by abarahho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libft/libft.h"
+
 typedef struct s_env
 {
-	char			*key;
-	char			*value;
-	struct s_env	*next;
-}	t_env;
+	char            *key;
+	char            *value;
+	struct s_env    *next;
+}   t_env;
 
-t_env	*create_env_node(char *entry)
+t_env   *create_env_node(char *entry)
 {
-	t_env	*new;
-	char	*sep;
+	t_env   *new;
+	char    *sep;
+
+	sep = ft_strchr(entry, '=');
+	if (!sep)
+		return (NULL);
 
 	new = malloc(sizeof(t_env));
 	if (!new)
 		return (NULL);
-	sep = ft_strchr(entry, '=');
-	if (!sep)
-	{
-		free(new);
-		return (NULL);
-	}
+
 	new->key = ft_strndup(entry, sep - entry);
 	new->value = ft_strdup(sep + 1);
+	new->next = NULL;
+
 	if (!new->key || !new->value)
 	{
 		free(new->key);
@@ -40,20 +46,25 @@ t_env	*create_env_node(char *entry)
 		free(new);
 		return (NULL);
 	}
-	new->next = NULL;
 	return (new);
 }
 
-t_env	*import_env(char **env)
+t_env   *import_env(char **env)
 {
-	t_env	*head;
-	t_env	*new;
-	t_env	*tmp;
+	t_env   *head = NULL;
+	t_env   *new;
+	t_env   *tmp;
+	int     i = 0;
 
-	head = NULL;
-	while (*env)
+	while (env[i])
 	{
-		new = create_env_node(*env);
+		new = create_env_node(env[i]);
+		if (!new)
+		{
+			i++;
+			continue;
+		}
+
 		if (!head)
 			head = new;
 		else
@@ -63,7 +74,7 @@ t_env	*import_env(char **env)
 				tmp = tmp->next;
 			tmp->next = new;
 		}
-		env++;
+		i++;
 	}
 	return (head);
 }
@@ -71,7 +82,6 @@ t_env	*import_env(char **env)
 void free_env(t_env *env)
 {
 	t_env *tmp;
-
 	while (env)
 	{
 		tmp = env;
@@ -82,18 +92,48 @@ void free_env(t_env *env)
 	}
 }
 
-int	main(int ac, char **av, char **env)
+void print_env(t_env *env)
 {
-	t_env	*our_env;
+	while (env)
+	{
+		printf("%s=%s\n", env->key, env->value);
+		env = env->next;
+	}
+}
+
+void unset_env(t_env **env, char *key)
+{
+	t_env   *tmp = *env;
+	t_env   *prev = NULL;
+
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->key, key) == 0)
+		{
+			if (prev)
+				prev->next = tmp->next;
+			else
+				*env = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			return;
+		}
+		prev = tmp;
+		tmp = tmp->next;
+	}
+}
+
+int main(int ac, char **av, char **envp)
+{
+	t_env   *our_env;
 
 	(void)ac;
 	(void)av;
-	our_env = import_env(env);
-	while (our_env)
-	{
-		printf("%s=%s\n", our_env->key, our_env->value);
-		our_env = our_env->next;
-	}
+
+	our_env = import_env(envp);
+	print_env(our_env);
 	free_env(our_env);
+	
 	return (0);
 }
