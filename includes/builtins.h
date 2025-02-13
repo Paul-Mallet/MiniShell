864c6a618f005be5ee2c22c4eb2d2f4a64fbc3d0
Adambrhh <adam.barahhou@proton.me>
2025-02-13 17:22:54 +0100

    feature: updated echo's functions and it used onlu to write str

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 11:43:19 by pamallet          #+#    #+#             */
-/*   Updated: 2025/02/13 15:56:52 by abarahho         ###   ########.fr       */
+/*   Updated: 2025/02/13 17:18:09 by abarahho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ void	ft_builtins(char *input, t_env *env);
 /*
 	echo
 */
-void	print_alias_value(t_env *env, char *name);
-char	*get_alias_name(char *str, int *i);
 void	ft_echo(char *str, t_env env, bool option_n);
 
 /*
--- src/exec/exec.h
+++ src/exec/exec.h
@@ -1,20 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   exec.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/12 11:39:36 by abarahho          #+#    #+#             */
-/*   Updated: 2025/02/13 17:20:08 by abarahho         ###   ########.fr       */
+/*   Created: 2025/02/13 17:21:03 by abarahho          #+#    #+#             */
+/*   Updated: 2025/02/13 17:21:04 by abarahho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
-#include "../../includes/builtins.h"
-
-void	ft_cd(char *path)
-{
-	if (chdir(path) == -1)
-		perror("cd"); //bash: cd: error_msg
-}
