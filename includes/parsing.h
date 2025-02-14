b9dbe056587d5c4915eff8e5a5094a221ba9d1e6
Adambrhh <adam.barahhou@proton.me>
2025-02-14 11:41:17 +0100

    Added lexer_utils's file for the the token lst

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,12 +6,22 @@
 /*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 08:43:05 by abarahho          #+#    #+#             */
-/*   Updated: 2025/02/14 10:44:42 by abarahho         ###   ########.fr       */
+/*   Updated: 2025/02/14 11:41:07 by abarahho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 # include "./minishell.h"
+
+t_token	*ft_lexer(char *input);
+
+/*
+	fonctions pour la liste chainee
+*/
+t_token	*new_token(t_token_first type, char *value);
+void	token_add_back(t_token **lst, t_token *new);
+void	free_token(t_token **token);
+
 
 #endif
--- src/parsing/lexer/lexer.c
+++ src/parsing/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 09:28:40 by abarahho          #+#    #+#             */
-/*   Updated: 2025/02/14 11:35:04 by abarahho         ###   ########.fr       */
+/*   Updated: 2025/02/14 11:39:01 by abarahho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,60 +30,4 @@ t_token	*ft_lexer(char *input)
 			tokens = is_a_word(input, &i);
     }
     return (tokens);
-}
-
-t_token	*ft_new_token(t_token_first type, char *value)
-{
-	t_token	*new;
-
-	new = (t_token *)malloc(sizeof(t_token));
-	if (!new)
-		return (NULL);
-	new->type = type;
-	new->value = ft_strdup(value);
-	if (!new->value)
-		return (free(new), NULL);
-	new->next = NULL;
-	return (new);
-}
-
-void	ft_token_add_back(t_token **lst, t_token *new)
-{
-	t_token	*current;
-
-	if (!lst || !new)
-		return ;
-	if (!*lst)
-	{
-		*lst = new;
-		return ;
-	}
-	current = *lst;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new;
-}
-
-void	ft_free_token(t_token *token)
-{
-	if (token->value)
-		free(token->value);
-	free(token);
-}
-void free_token(t_token **token)
-{
-	t_env	*tmp;
-	t_env	*next;
-
-	if (token == NULL)
-		return ;
-	tmp = *token;
-	while (tmp != NULL)
-	{
-		next = tmp->next;
-		free(tmp->value);
-		free(tmp);
-		tmp = next;
-	}
-	*token = (NULL);
 }
