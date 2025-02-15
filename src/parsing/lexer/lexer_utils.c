d8a3770a66bff58c872ef7cc2fec05e3501df27b
paul_mallet <paulhavre42@gmail.com>
2025-02-15 11:55:05 +0100

    feat: removed ex token's functions from lexer_utils

--- src/parsing/lexer/lexer_utils.c
+++ src/parsing/lexer/lexer_utils.c
@@ -3,62 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   lexer_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paul_mallet <paul_mallet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 11:38:28 by abarahho          #+#    #+#             */
-/*   Updated: 2025/02/14 11:39:15 by abarahho         ###   ########.fr       */
+/*   Updated: 2025/02/15 11:54:27 by paul_mallet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
-#include "../../includes/parsing.h"
-
-t_token	*new_token(t_token_first type, char *value)
-{
-	t_token	*new;
-
-	new = (t_token *)malloc(sizeof(t_token));
-	if (!new)
-		return (NULL);
-	new->type = type;
-	new->value = ft_strdup(value);
-	if (!new->value)
-		return (free(new), NULL);
-	new->next = NULL;
-	return (new);
-}
-
-void	token_add_back(t_token **lst, t_token *new)
-{
-	t_token	*current;
-
-	if (!lst || !new)
-		return ;
-	if (!*lst)
-	{
-		*lst = new;
-		return ;
-	}
-	current = *lst;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new;
-}
-
-void free_token(t_token **token)
-{
-	t_env	*tmp;
-	t_env	*next;
-
-	if (token == NULL)
-		return ;
-	tmp = *token;
-	while (tmp != NULL)
-	{
-		next = tmp->next;
-		free(tmp->value);
-		free(tmp);
-		tmp = next;
-	}
-	*token = (NULL);
-}
+#include "../../../includes/minishell.h"
+#include "../../../includes/parsing.h"
