40d4a5f823a6213d892d86a761c3bb83dfa18cfc
Adambrhh <adam.barahhou@proton.me>
2025-02-17 10:47:20 +0100

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:55:18 by pamallet          #+#    #+#             */
-/*   Updated: 2025/02/16 18:52:53 by abarahho         ###   ########.fr       */
+/*   Updated: 2025/02/17 10:15:28 by abarahho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int ac, char **av, char **envp)
 	t_data	*data;
 	(void)ac;
 
-	init_data(data)
+	init_data(data);
 	env = import_env(envp);
 	str = expander(av[1], env);
 	print_export(env);
