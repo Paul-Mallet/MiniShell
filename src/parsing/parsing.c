58aad849ecf6fbe678593b71d1ab572092cb6be3
paul_mallet <paulhavre42@gmail.com>
2025-02-18 09:25:25 +0100

    feat: modified reduce_spaces to fit quotes conditions

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paul_mallet <paul_mallet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 17:17:36 by pamallet          #+#    #+#             */
-/*   Updated: 2025/02/17 16:40:22 by abarahho         ###   ########.fr       */
+/*   Updated: 2025/02/17 23:21:04 by paul_mallet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,34 @@ static char	*ft_reduce_spaces(char *input)
 	char	*str;
 	int		i;
 	int		j;
+	int		quote;
 
 	str = (char *)malloc((ft_strlen(input) + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
 	i = 0;
 	j = 0;
+	quote = 0;
 	while (input[i])
 	{
-		while (!ft_isspace(input[i]) && input[i])
-			str[j++] = input[i++];
-		while (ft_isspace(input[i]) && input[i])
-			i++;
-		if (input[i])
-			str[j++] = ' ';
+		if ((input[i] == '"' || input[i] == '\'') && quote == 0)
+			quote = input[i];
+		else if (input[i] == quote)
+			quote = 0;
+		str[j++] = input[i++];
+		if (!quote)
+		{
+			while (ft_isspace(input[i]) && input[i])
+				i++;
+			if (input[i] && !ft_isspace(str[j - 1]))
+				str[j++] = ' ';
+		}
+		// while (!ft_isspace(input[i]) && input[i])
+		// 	str[j++] = input[i++];
+		// while (ft_isspace(input[i]) && input[i])
+		// 	i++;
+		// if (input[i])
+		// 	str[j++] = ' ';
 	}
 	str[j] = '\0';
 	return (str);
@@ -52,8 +66,8 @@ t_token	*ft_parsing(char *input)
 	char	*reduced;
 	t_token	*tokens;
 
-	trimmed = ft_strtrim(input, " \t\n\v\f\r");
-	reduced = ft_reduce_spaces(trimmed);
+	trimmed = ft_strtrim(input, " \t\n\v\f\r"); //"  ...   "
+	reduced = ft_reduce_spaces(trimmed); //". . ."
 	tokens = first_tokenization(trimmed);
 	free(trimmed);
 	free(reduced);
